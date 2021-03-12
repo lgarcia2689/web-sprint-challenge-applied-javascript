781dfa73347fb4fb932023d526363ba2b4da6fbb
unknown <you@example.com>
2021-03-12 13:39:40 -0600

    created header, tabs, and cards

--- src/components/card.js
+++ src/components/card.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 const Card = (article) => {
   // TASK 5
   // ---------------------
@@ -17,6 +19,29 @@ const Card = (article) => {
   //   </div>
   // </div>
   //
+  const cardDiv = document.createElement('div');
+  const headlineDiv  = document.createElement('div');
+  const author = document.createElement('div');
+  const imgContainer = document.createElement('div');
+  const imageSrc = document.createElement('img');
+  const authorSpan = document.createElement('span');
+
+  cardDiv.className = 'card';
+  headlineDiv.className = 'headline';
+  author.className = 'author';
+  imgContainer.className = 'img-container';
+
+  cardDiv.appendChild(headlineDiv);
+  cardDiv.appendChild(author);
+  author.appendChild(imgContainer);
+  imgContainer.appendChild(imageSrc);
+  author.appendChild(authorSpan);
+
+      headlineDiv.textContent=article.headline;
+      authorSpan.textContent = article.authorName;
+      imageSrc.setAttribute('src', article.authorPhoto);
+
+  return cardDiv
 }
 
 const cardAppender = (selector) => {
@@ -28,6 +53,44 @@ const cardAppender = (selector) => {
   // Create a card from each and every article object in the response, using the Card component.
   // Append each card to the element in the DOM that matches the selector passed to the function.
   //
+  const cardsContainer = document.querySelector(selector)
+
+  axios
+  .get('https://lambda-times-api.herokuapp.com/articles')
+  .then((res) => {
+
+    const bootstrap = res.data.articles.bootstrap;
+    const javascript = res.data.articles.javascript;
+    const jquery = res.data.articles.jquery;
+    const node = res.data.articles.node;
+    const technology = res.data.articles.technology;
+
+    console.log(res.data.articles)
+    
+    cardsContainer.append(Card(bootstrap[0]))
+    cardsContainer.append(Card(bootstrap[1]))
+    cardsContainer.append(Card(bootstrap[2]))
+
+    cardsContainer.append(Card(javascript[0]))
+    cardsContainer.append(Card(javascript[1]))
+    cardsContainer.append(Card(javascript[2]))
+    cardsContainer.append(Card(javascript[3]))
+
+    cardsContainer.append(Card(jquery[0]))
+    cardsContainer.append(Card(jquery[1]))
+    cardsContainer.append(Card(jquery[2]))
+
+    cardsContainer.append(Card(node[0]))
+    cardsContainer.append(Card(node[1]))
+
+    cardsContainer.append(Card(technology[0]))
+    cardsContainer.append(Card(technology[1]))
+    cardsContainer.append(Card(technology[2]))
+    
+  })
+  .catch((err) => {
+    console.log(err)
+  })
 }
 
 export { Card, cardAppender }
--- src/components/header.js
+++ src/components/header.js
@@ -1,25 +1,28 @@
 const Header = (title, date, temp) => {
-  // TASK 1
-  // ---------------------
-  // Implement this function taking `title`, `date` and `temp` as its 3 args and returning the markup below.
-  // The tags used, the hierarchy of elements and their attributes must match the provided markup exactly!
-  // The text inside elements will be set using their `textContent` property (NOT `innerText`).
-  //
-  //  <div class="header">
-  //    <span class="date">{ date }</span>
-  //    <h1>{ title }</h1>
-  //    <span class="temp">{ temp }</span>
-  //  </div>
-  //
+  const headerDiv = document.createElement('div');
+  const dateSpan = document.createElement('span');
+  const titleh1 = document.createElement('h1');
+  const tempSpan = document.createElement('span');
+
+  headerDiv.className = 'header';
+  dateSpan.className = 'date';
+  tempSpan.className = 'temp'
+
+  dateSpan.textContent = date;
+  titleh1.textContent = title;
+  tempSpan.textContent = temp;
+
+  headerDiv.appendChild(dateSpan);
+  headerDiv.appendChild(titleh1);
+  headerDiv.appendChild(tempSpan);
+
+  return headerDiv;
 }
 
 const headerAppender = (selector) => {
-  // TASK 2
-  // ---------------------
-  // Implement this function taking a css selector as its only argument.
-  // It should create a header using the Header component above, passing arguments of your choosing.
-  // It should append the header to the element in the DOM that matches the given selector.
-  //
+  const headerContainer = document.querySelector(selector)
+  
+  headerContainer.append(Header('Luis Garcia','03/12/2021','80°'))
 }
 
 export { Header, headerAppender }
